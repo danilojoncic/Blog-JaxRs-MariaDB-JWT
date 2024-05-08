9ee944fff22b6ec4ac76309f886b4d26983f427e
danilojoncic <djoncic14122rn@raf.rs>
2024-05-08 15:11:17 +0200

    user service and filter done

--- src/main/java/com/example/hw/filter/RequestFilter.java
+++ src/main/java/com/example/hw/filter/RequestFilter.java
@@ -1,4 +1,53 @@
 package com.example.hw.filter;
 
-public class RequestFilter {
+import com.example.hw.resource.PostResource;
+import com.example.hw.service.UserService;
+
+import javax.inject.Inject;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.List;
+
+public class RequestFilter implements ContainerRequestFilter {
+    @Inject
+    UserService userService;
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+//        if (true) return;
+
+        if (!this.isAuthRequired(requestContext)) {
+            return;
+        }
+
+        try {
+            String token = requestContext.getHeaderString("Authorization");
+            if(token != null && token.startsWith("Bearer ")) {
+                token = token.replace("Bearer ", "");
+            }
+
+            if (!this.userService.isAuthorized(token)) {
+                requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+            }
+        } catch (Exception exception) {
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+        }
+    }
+
+    private boolean isAuthRequired(ContainerRequestContext req) {
+        if (req.getUriInfo().getPath().contains("login")) {
+            return false;
+        }
+
+        List<Object> matchedResources = req.getUriInfo().getMatchedResources();
+        for (Object matchedResource : matchedResources) {
+            if (matchedResource instanceof PostResource) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- src/main/java/com/example/hw/service/UserService.java
+++ src/main/java/com/example/hw/service/UserService.java
@@ -1,4 +1,41 @@
 package com.example.hw.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.hw.domain.User;
+import com.example.hw.repository.user.UserRepository;
+
+import javax.inject.Inject;
+import java.util.Date;
+
 public class UserService {
+    @Inject
+    UserRepository userRepository;
+
+
+    //ovo pravi token
+    public String login(String username, String password){
+        User user = this.userRepository.findUser(username);
+        if(user == null || !user.getPassword().equals(password))return null;
+
+        Date issuedAt = new Date();
+        //od vremena izdavanja dodaj jedan dan ekvivalentan milisekundama
+        Date expiresAt = new Date(issuedAt.getTime() + 24 * 60 * 60 * 1000);
+        Algorithm algorithm = Algorithm.HMAC256("ultra tajna sifra");
+        return JWT.create().withIssuedAt(issuedAt).withExpiresAt(expiresAt).withSubject(username).sign(algorithm);
+    }
+
+
+    //ovo cita token i provjerava da li je validan
+    public boolean isAuthorized(String token){
+        Algorithm algorithm = Algorithm.HMAC256("ultra tajna sifra");
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT jwt = verifier.verify(token);
+
+        String username = jwt.getSubject();
+        User user = this.userRepository.findUser(username);
+        return user != null;
+    }
 }
